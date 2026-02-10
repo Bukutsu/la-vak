563954906a5efd3934a0caa0c8c6ee34edea0a81
Carmel-1311 <nightfish1596@gmail.com>
2026-02-10 16:08:47 +0700

    main next

--- client/.next/server/middleware-build-manifest.js
+++ client/.next/server/middleware-build-manifest.js
@@ -2,35 +2,13 @@ globalThis.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
-<<<<<<< HEAD
-  "devFiles": [
-    "static/chunks/react-refresh.js"
-  ],
-=======
   "devFiles": [],
->>>>>>> phat
   "ampDevFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/webpack.js",
-    "static/chunks/main-app.js"
-  ],
+  "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
-<<<<<<< HEAD
-    "/_app": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_app.js"
-    ],
-    "/_error": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_error.js"
-    ]
-=======
     "/_app": []
->>>>>>> phat
   },
   "ampFirstPages": []
 };
